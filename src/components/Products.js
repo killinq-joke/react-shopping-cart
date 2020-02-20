a9235c68bba88e424d12b6a9cde3b8fa728c4ce3
Zak <zakadmaster@gmail.com>
2020-02-20 14:23:14 +0100

    Addition of function addItem

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,19 @@
-import React from 'react';
+import React, { useContext } from "react";
+import productContext from "../contexts/products";
 
 // Components
-import Product from './Product';
+import Product from "./Product";
 
-const Products = props => {
-	return (
-		<div className="products-container">
-			{props.products.map(product => (
-				<Product
-					key={product.id}
-					product={product}
-					addItem={props.addItem}
-				/>
-			))}
-		</div>
-	);
+const Products = ({products, addItem}) => {
+	// const products = useContext(productContext)
+	// console.log(products)
+  return (
+    <div className="products-container">
+      {products.map(product => (
+        <Product key={product.id} product={product} addItem={addItem} />
+      ))}
+    </div>
+  );
 };
 
 export default Products;
